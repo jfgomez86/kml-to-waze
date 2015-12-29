0f7588f6bf03f97da88887e67828bc7e594a2102
Jose Felix Gomez <jfgomez86@gmail.com>
2015-12-28 20:55:58 -0500

    Correction for lat,lon in xml. Reversing coordinates

--- lib/place.rb
+++ lib/place.rb
@@ -7,7 +7,7 @@ class Place < Sequel::Model
 
   def parse_attributes(xmlplacemark)
     self[:name]             = xmlplacemark.css("name").text
-    self[:coordinates]      = xmlplacemark.css("coordinates").text
+    self[:coordinates]      = xmlplacemark.css("coordinates").text.split(",")[0..1].reverse.join(",")
     self[:waze_url]         = "waze://?ll=#{self[:coordinates]}&navigate=yes"
     self[:google_maps_url]  = "comgooglemaps://?daddr=#{self[:coordinates]}&directionsmode=driving"
   end
